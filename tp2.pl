282622af36755d2178403b1fdb952c627c0444d3
Catherine Asselin <catherine.asselin.6@ulaval.ca>
2018-03-08 00:52:40 -0500

    Fix columns/row issue

--- tp2.pl
+++ tp2.pl
@@ -3,6 +3,8 @@
 	- : il n'y a aucun jeton sur la case
 	x : jeton du player
 	o : jeton du IA
+	item 0 : haut de la colone
+	item 6 : bas de la colone
 */
 etat_initial(board([[-, -, -, -, -, -],
 										[-, -, -, -, -, -],
@@ -48,12 +50,17 @@ etat_final(board([[A, A, A, A, A, A],
 									n) :- not(A = -)).
 
 /* Print board functions */
+show_board(board(X)):- show(X,6), print_colomns_number.
 
-print_board([]).
-print_board([A|B]) :- print_board(B),format('  ~w  ~w  ~w  ~w  ~w  ~w  ~w ~n', A).
-print_colomns() :- write('\n  1  2  3  4  5  6  7\n\n').
+show(_,0).
+show(X,N):- print_colomn(X,X2), nl, Ns is N-1, show(X2,Ns).
 
-/*  Add a coin in the row */
+print_colomn([],_).
+print_colomn([[X|X2]|XS],[X2|XS2]):- write(X), write('  '), print_colomn(XS,XS2).
+
+print_colomns_number :- write('1  2  3  4  5  6  7').
+
+/*  Validate user entry */
 colomn(1).
 colomn(2).
 colomn(3).
@@ -62,31 +69,16 @@ colomn(5).
 colomn(6).
 colomn(7).
 colomn_available(X) :- colomn(X).
-add_coin(1) :- write('IA wins!\n').
 
 /* Turn to play */
 turn_to_play(x) :- write('Your turn.\n').
 turn_to_play(o) :- write('IA\'s turn.\n').
 
 /* Who wins? */
-wins(1) :- write('IA wins!\n').
-wins(0) :- write('You win!\n').
-wins(2) :- write('Tie\n').
+print_winner(1) :- write('IA wins!\n').
+print_winner(0) :- write('You win!\n').
+print_winner(2) :- write('Tie\n').
+winner(X, Y) :- etat_final(X), print_winner(Y).
 
 /* Play the game :) */
-
-play() :- print_board([[x, x, o, x, x, o, o],
-											[-, x, o, o, x, -, o],
-											[-, -, x, o, -, -, -],
-											[-, -, -, x, -, -, -],
-											[-, -, -, -, -, -, -],
-											[-, -, -, -, -, -, -]]),
-											print_colomns().
-
-play(A) :- row(A), print_board([[x, x, o, x, x, o, o],
-																[-, x, o, o, x, -, o],
-																[-, -, x, o, -, -, -],
-																[-, -, -, x, -, -, -],
-																[-, -, -, -, -, -, -],
-																[-, -, -, -, -, -, -]], x),
-																print_colomns().
+play :- etat_initial(X), show_board(X).
